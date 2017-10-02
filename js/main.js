40049b8e7883eb23f8d91a22cc12ccbc1b588532
Nikita <hik94dh@mail.ru>
2017-10-03 00:13:53 +0300

    Переписал pressKey, поменял let на const, удалил лишние пробелы

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
 const central = document.querySelector(`.central`);
 const templates = document.querySelectorAll(`template`);
 let currentScreen = 0;
-let lastScreen = 5;
-
+const lastScreen = templates.length;
 const ARROW_LEFT = 37;
 const ARROW_RIGHT = 39;
-
 
 const showScreen = (index) => {
   central.innerHTML = templates[index].innerHTML;
 };
 
-
 const pressKey = (e) => {
-    if (e.altKey && e.keyCode === ARROW_LEFT && currentScreen > 0) {
-        --currentScreen;
+    if (e.altKey) {
+        if (this && e.keyCode === ARROW_LEFT && currentScreen > 0) {
+            --currentScreen;
+            showScreen(currentScreen);
+        }
+        else if (this && e.keyCode === ARROW_RIGHT && currentScreen < lastScreen - 1) {
+            ++currentScreen;
+            showScreen(currentScreen);
+        }
     }
-    else if (e.altKey && e.keyCode === ARROW_RIGHT && currentScreen < lastScreen) {
-        ++currentScreen;
-    }
-    showScreen(currentScreen);
 };
-
-
-showScreen(currentScreen);
-
 
 document.addEventListener(`keydown`, pressKey);
