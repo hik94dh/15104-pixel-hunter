2d8c0f30bbf240b625f7546bee23b5da89fb34f5
Nikita <hik94dh@mail.ru>
2017-10-16 19:57:41 +0300

    убрал массивы в кейсы и добавил вариант с разной скоростью

--- js/data/score.test.js
+++ js/data/score.test.js
@@ -2,150 +2,159 @@ import assert from 'assert';
 import countTotal from './score';
 import {timeType} from './game-config';
 
-
-const testScore = {
-  lives: 0,
-  answers: [
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL}
-  ]
-};
-
-const testScore2 = {
-  lives: 0,
-  answers: [
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL}
-  ]
-};
-
-const testScore3 = {
-  lives: 3,
-  answers: [
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL}
-  ]
-};
-
-const testScore4 = {
-  lives: 3,
-  answers: [
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW}
-  ]
-};
-
-const testScore5 = {
-  lives: 0,
-  answers: [
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW},
-    {correct: true, timeType: timeType.SLOW}
-  ]
-};
-
-const testScore6 = {
-  lives: 3,
-  answers: [
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST}
-  ]
-};
-
-const testScore7 = {
-  lives: 0,
-  answers: [
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST},
-    {correct: true, timeType: timeType.FAST}
-  ]
-};
-
-const testScore8 = {
-  lives: 1,
-  answers: [
-    {correct: false, timeType: timeType.NORMAL},
-    {correct: false, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL},
-    {correct: true, timeType: timeType.NORMAL}
-  ]
-};
-
 describe(`Score`, () => {
   it(`Если ответов меньше 10, то вернёт -1`, () => {
+    const testScore = {
+      lives: 0,
+      answers: [
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.FAST}
+      ]
+    };
     assert.equal(-1, countTotal(testScore));
   });
   it(`Если всё верно и нет бонусов`, () => {
-    assert.equal(1000, countTotal(testScore2));
+    const testScore2 = {
+      lives: 0,
+      answers: [
+        {correct: false, timeType: timeType.NORMAL},
+        {correct: false, timeType: timeType.NORMAL},
+        {correct: false, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL}
+      ]
+    };
+    assert.equal(700, countTotal(testScore2));
   });
   it(`Если всё верно и остались 3 жизни`, () => {
+    const testScore3 = {
+      lives: 3,
+      answers: [
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL}
+      ]
+    };
     assert.equal(1150, countTotal(testScore3));
   });
   it(`Если всё верно, но медленно и остались 3 жизни`, () => {
+    const testScore4 = {
+      lives: 3,
+      answers: [
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW}
+      ]
+    };
     assert.equal(650, countTotal(testScore4));
   });
   it(`Если всё верно, но медленно и нет бонусов`, () => {
-    assert.equal(500, countTotal(testScore5));
+    const testScore5 = {
+      lives: 0,
+      answers: [
+        {correct: false, timeType: timeType.SLOW},
+        {correct: false, timeType: timeType.SLOW},
+        {correct: false, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.SLOW}
+      ]
+    };
+    assert.equal(350, countTotal(testScore5));
   });
   it(`Если всё верно + быстро и остались 3 жизни`, () => {
+    const testScore6 = {
+      lives: 3,
+      answers: [
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST}
+      ]
+    };
     assert.equal(1650, countTotal(testScore6));
   });
   it(`Если всё верно + быстро и нет бонусов`, () => {
-    assert.equal(1500, countTotal(testScore7));
+    const testScore7 = {
+      lives: 0,
+      answers: [
+        {correct: false, timeType: timeType.FAST},
+        {correct: false, timeType: timeType.FAST},
+        {correct: false, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.FAST}
+      ]
+    };
+    assert.equal(1050, countTotal(testScore7));
   });
   it(`Если 2 ошибки и осталась 1 жизнь`, () => {
+    const testScore8 = {
+      lives: 1,
+      answers: [
+        {correct: false, timeType: timeType.NORMAL},
+        {correct: false, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.NORMAL}
+      ]
+    };
     assert.equal(850, countTotal(testScore8));
+  });
+  it(`Если разная скорость и 2 жизни`, () => {
+    const testScore9 = {
+      lives: 2,
+      answers: [
+        {correct: false, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.FAST},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.SLOW},
+        {correct: true, timeType: timeType.NORMAL},
+        {correct: true, timeType: timeType.FAST}
+      ]
+    };
+    assert.equal(1000, countTotal(testScore9));
   });
 });
